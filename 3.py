ea83d9461c5568e19d8d801ab06f220c51be8c89
Nagasai143 <61235918+Nagasai143@users.noreply.github.com>
2022-11-21 16:35:55 +0530

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,11 @@
+def nagasai(m):
+    l=[]
+    count = 0
+    for i in range(0,10000):
+        nSet = set(list(str(i)))
+        if (not nSet.intersection({'3','4','7'}) and 
+                nSet.intersection({'1','0','2','5','6','8','9'})):
+            l.append(i)  
+    return l[m]
+m=int(input())
+print(nagasai(m))
